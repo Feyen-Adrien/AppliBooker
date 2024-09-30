bf4b6776710dd7d7cbf7e715dedf60d6053be989
Feyen-Adrien <feyenadrien2002@gmail.com>
2024-09-30 10:45:08 +0200

    :valid_check: cmakefile

---
+++ ClientBookEncoderQt/build/CMakeFiles/ApplicBooker.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/HEPL2024-2025/RTI/Labo/Eval1/Booker/ClientBookEncoderQt/src/main.cpp" "/home/student/HEPL2024-2025/RTI/Labo/Eval1/Booker/ClientBookEncoderQt/build/CMakeFiles/ApplicBooker.dir/src/main.cpp.o"
+  "/home/student/HEPL2024-2025/RTI/Labo/Eval1/Booker/ClientBookEncoderQt/src/mainwindowclientbookencoder.cpp" "/home/student/HEPL2024-2025/RTI/Labo/Eval1/Booker/ClientBookEncoderQt/build/CMakeFiles/ApplicBooker.dir/src/mainwindowclientbookencoder.cpp.o"
+  "/home/student/HEPL2024-2025/RTI/Labo/Eval1/Booker/ClientBookEncoderQt/src/moc_mainwindowclientbookencoder.cpp" "/home/student/HEPL2024-2025/RTI/Labo/Eval1/Booker/ClientBookEncoderQt/build/CMakeFiles/ApplicBooker.dir/src/moc_mainwindowclientbookencoder.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/mkspecs/linux-g++"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtWidgets"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ClientBookEncoderQt/build/CMakeFiles/ApplicBooker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ApplicBooker"
+  "ApplicBooker.pdb"
+  "CMakeFiles/ApplicBooker.dir/src/main.cpp.o"
+  "CMakeFiles/ApplicBooker.dir/src/mainwindowclientbookencoder.cpp.o"
+  "CMakeFiles/ApplicBooker.dir/src/moc_mainwindowclientbookencoder.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ApplicBooker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
